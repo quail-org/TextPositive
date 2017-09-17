6134978945c0f194bd0f76aab62ee4de567c943b
Asher Trockman <asher.trockman@gmail.com>
2017-09-17 10:56:53 -0400

    thresh

--- Chrome Extension/src/inject/inject.js
+++ Chrome Extension/src/inject/inject.js
@@ -226,7 +226,7 @@ function bind() {
 			} catch(e) {
 				s = 0;
 			}
-			if(s > 0.5 && e.pageX >= r.left && e.pageX <= r.right
+			if(s > 0.45 && e.pageX >= r.left && e.pageX <= r.right
 				&& e.pageY >= ry && e.pageY <= ry2) {
 				STATUS.style.visibility = "visible";
 				STATUS.style.top = (ry2 + 0) + 'px';
@@ -265,7 +265,7 @@ function updateClasses() {
 		}
 		let elem = document.getElementById(id);
 
-		if(elem && res.data[0][0] > 0.5 && elem.className != 'negative') {
+		if(elem && res.data[0][0] > 0.45 && elem.className != 'negative') {
 			elem.className = 'negative';
 		}
 	});
